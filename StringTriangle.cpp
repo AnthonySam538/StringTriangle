6f62d59c9c1f6eea99a2bc39543400056b43822c
AnthonySam538 <42751473+AnthonySam538@users.noreply.github.com>
2018-10-23 08:57:17 -0700

    Would the sequel to KINGDOM HEARTS 358/2 Days be KINGDOM HEARTS 358 Days?
    Some minor changes

--- StringTriangle.cpp
+++ StringTriangle.cpp
@@ -8,36 +8,35 @@ int main()
 {
   //declare necessary variables
   string myString;
-	ofstream outfile;
+  ofstream outfile;
 
-  //Retrieve and store the string
+  //Retrieve and store the user's string
   cout << "Enter your string: ";
   getline(cin, myString);
 
   //open (or create) the output file
   outfile.open("triangle.txt");
-
+  
+  
   //top half of the triangle (including the phrase)
   for(int x = 0; x < myString.length(); x++)
   {
     if(isspace(myString[x])) x++; //skips "duplicate" rows (distinguishable only by whitespace)
 
     for(int y = 0; y < x + 1; y++) //amount of characters printed: row+1
-    {
       outfile << myString[y];
-    }
+    
     outfile << endl; //move to next row/line
   }
 
   //bottom half of the triangle
   for(int x = myString.length() - 1; x > 0; x--)
   {
-    if(isspace(myString[x])) x--; //also for skipping "duplicate" rows
+    if(isspace(myString[x])) x--; //also prevents "duplicate" rows
 
     for(int y = 0; y < x; y++) //the amount of characters printed depends on the current row
-    {
       outfile << myString[y];
-    }
+    
     outfile << endl; //move to next row/line
   }
 
